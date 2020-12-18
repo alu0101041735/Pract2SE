509ceaf0478c3ec15d93ada2df932ae102cb1c67
0x444d4d <alu0101053318@ull.edu.es>
2020-12-18 11:52:45 +0000

    update 7seg

--- tests/test_7segments.c
+++ tests/test_7segments.c
@@ -4,72 +4,103 @@
 #include <sys/ports.h>
 #include <sys/sio.h>
 
+void sieteSeg_init();
+void sieteSeg_digitos(uint8_t*);
+void sieteSeg_valor(uint16_t);
+void update_val();
+void refresh_display();
 
-int *readPort(uint8_t port)
-{
+//int *readPort(uint8_t);
+//void setNumber(uint8_t number[8], uint8_t port, uint8_t segment, uint8_t axsegments);
 
-  static int read[8];
+typedef struct display_data {
+	uint8_t value_array[4];
+	uint8_t queued_value_array[4];
+	uint8_t current_digit;
+	uint8_t current_display;
+} display_data;
 
-  read[0] = gpio_read_pin(port, 0).data;
-  read[1] = gpio_read_pin(port, 1).data;
-  read[2] = gpio_read_pin(port, 2).data;
-  read[3] = gpio_read_pin(port, 3).data;
-  read[4] = gpio_read_pin(port, 4).data;
-  read[5] = gpio_read_pin(port, 5).data;
-  read[6] = gpio_read_pin(port, 6).data;
-  read[7] = gpio_read_pin(port, 7).data;
-
-  return read;
-  
-}
-
-
-void setNumber(int number[8], int port, int segment, int maxsegments)
-{
-  int i;
-
-  for (i = 0; i < maxsegments; i++) {
-    if (i == segment) {
-      gpio_write_pin(port, i, 1);
-    }
-    else {
-      gpio_write_pin(port, i, 0);
-    }
-  }
-
-  int pins[4];
-  pins[0] = number[4];
-  pins[1] = number[5];
-  pins[2] = number[6];
-  pins[3] = number[7];
-
-
-  gpio_write_pin(port, 4, pins[0]);
-  gpio_write_pin(port, 5, pins[1]);
-  gpio_write_pin(port, 6, pins[2]);
-  gpio_write_pin(port, 7, pins[3]);
-
-}
-
+display_data display;
 
 int main() 
 {
-  serial_init();
-  gpio_pup_disable_(M6812_PORTH);
-  gpio_set_output_all_reg(SET_PIN_H);
-  gpio_set_input_all_reg(SET_PIN_G);
+	uint8_t digitos[4];
+	sieteSeg_init();
+	//init timer
+	timer_init(3);
+	//refresh every 10ms
+	timer_schelude_call(10000, refresh_display);
+	//update value every 2ms
+	timer_schedule_call(500000, update_val);
 
-  int segment = 0;
-  int max_segment_number = 4;
+	while (1) {
+		//leer teclado y pedir numero
+		digitos[0] = 1;
+		digitos[1] = 2;
+		digitos[2] = 3;
+		digitos[3] = 4;
+		sieteSeg_digitos(digitos);
+	}
+}
 
-  while(1) {
-    int *number;
-    number = readPort(M6812_PORTG);
-    setNumber(number, M6812_PORTH, segment, max_segment_number);
 
-    ++segment;
+void sieteSeg_init() {
+	display.current_value = 0;
+	display.queued_value = 0;
+	display.current_display = 0x01;
 
-    if (segment >= max_segment_number)
-      segment = 0;
-  }
+	gpio_pup_disable(M6812_PORTG);
+	gpio_set_output_all_reg(M6812_PORTG);
+}
+
+
+void sieteSeg_digitos(uint8_t* new_digits) {
+	uint8_t mask = 0x0F;
+	display.queued_value_array[0] = new_digits[0] & mask;
+	display.queued_value_array[1] = new_digits[1] & mask;
+	display.queued_value_array[2] = new_digits[2] & mask;
+	display.queued_value_array[3] = new_digits[3] & mask;
+}
+
+void sieteSeg_valor(uint16_t newValue) {
+	uint16_t temp = newValue;
+	uint8_t index = 0;
+	while (temp > 0 && index < 4) {
+		display.queued_value_array[index] = temp % 10;
+		++index;
+		temp /= 10;
+	}
+}
+
+
+void update_val() {
+	display.queued_value =  newValue;
+	uint8_t* aux_ptr = display.value_array;
+	display.value_array = display.queued_value_array;
+	display.queued_value_array = aux_ptr;
+}
+
+
+void refresh_display() {
+	if (current_display > 8) {
+		current_display = 0x01;
+		display.current_digit = 0;
+	}
+
+	/* The port_data variable will hold the 7segment
+	 * display to ve enabled on it's 4 most significant bits
+	 * and the number to display on the other 4 bits.
+	 * 			       selected 
+	 *				 		 display  digit
+	 * port_data = xxxx     xxxx
+	 */
+	uint8_t digit = display.value_array[display.current_digit];
+	digit &= 0x0F;
+
+	uint8_t port_data = (current_display << 4) | digit;
+
+	gpio_write_port(port_data);
+	display.current_display = display.current_display << 0x01;
+	++display.current_digit;
+
 }
